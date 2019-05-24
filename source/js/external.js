3c5485f3cfc1dc2fcde399af30a657439a6c33ea
zinchandrey <zinchandrey@gmail.com>
2019-05-25 00:21:20 +0300

    Добавляет ссылки на кнопки Заказать и Показать все

--- source/js/external.js
+++ source/js/external.js
@@ -1,5 +1,6 @@
 var link = document.querySelector(".main-nav__toggle");
 var searchPopup = document.querySelector(".main-nav__list--show");
+var orderLink = document.querySelector(".order-link");
 
 searchPopup.classList.remove("main-nav__list--show");
 searchPopup.classList.add("main-nav__list--close");
@@ -7,4 +8,8 @@ searchPopup.classList.add("main-nav__list--close");
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
     searchPopup.classList.toggle("main-nav__list--close");
+});
+
+orderLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
 });
