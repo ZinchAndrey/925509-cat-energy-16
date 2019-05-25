748c5d65546bbd622ac850f6e11361acd2a795cd
zinchandrey <zinchandrey@gmail.com>
2019-05-26 00:11:09 +0300

    Исправляет критерии Б5 Б8 Б9 Б12 Б26

--- source/js/external.js
+++ source/js/external.js
@@ -1,6 +1,10 @@
 var link = document.querySelector(".main-nav__toggle");
 var searchPopup = document.querySelector(".main-nav__list--show");
+var searchToggle = document.querySelector(".main-nav__toggle--disable");
 var orderLinks = document.querySelectorAll(".order-link");
+
+searchToggle.classList.remove("main-nav__toggle--disable");
+searchToggle.classList.add("main-nav__toggle");
 
 searchPopup.classList.remove("main-nav__list--show");
 searchPopup.classList.add("main-nav__list--close");
